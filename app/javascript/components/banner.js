371dc1a193cdb3019f8672bdb9dc0111962f9b22
felippe menezes <felippemenezes1982@gmail.com>
2021-05-18 10:33:21 -0300

    updating details of typed.js

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,11 +2,11 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Ask ^350 your ^250 Coach ^350 anything:", "Pergunte ^250 qualquer ^350 coisa ^250 ao ^150 seu ^250 Coach:"],
+    strings: ["Ask ^350 your ^250 Coach ^350 anything:", " ^1000 Pergunte ^250 qualquer ^350 coisa ^250 ao ^150 seu ^250 Coach:"],
     typeSpeed: 75,
     backSpeed: 50,
     loop: true,
-    startDelay: 2000,
+    startDelay: 1000,
     backDelay: 3000
   });
 }
