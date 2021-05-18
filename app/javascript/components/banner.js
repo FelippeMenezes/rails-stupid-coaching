2295c04bce32aa17b5d753f00c85a6f78b9104ad
felippe menezes <felippemenezes1982@gmail.com>
2021-05-18 10:23:46 -0300

    updating typed.js

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -5,7 +5,9 @@ const loadDynamicBannerText = () => {
     strings: ["Ask ^350 your ^250 Coach ^350 anything:", "Pergunte ^250 qualquer ^350 coisa ^250 ao ^150 seu ^250 Coach:"],
     typeSpeed: 75,
     backSpeed: 50,
-    loop: true
+    loop: true,
+    startDelay: 2000,
+    backDelay: 3000
   });
 }
 
