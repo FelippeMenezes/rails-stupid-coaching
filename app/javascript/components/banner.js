8bfc1089401ad47e4746c0963065f686246455fa
felippe menezes <felippemenezes1982@gmail.com>
2021-05-18 10:16:30 -0300

    updating some animation

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,10 +2,10 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Ask your Coach anything:", "Pergunte qualquer coisa ao seu Coach:"],
-    typeSpeed: 50,
-    loop: true,
-    backSpeed: 25
+    strings: ["Ask ^350 your ^250 Coach ^350 anything:", "Pergunte ^250 qualquer ^350 coisa ^250 ao ^150 seu ^250 Coach:"],
+    typeSpeed: 75,
+    backSpeed: 50,
+    loop: true
   });
 }
 
