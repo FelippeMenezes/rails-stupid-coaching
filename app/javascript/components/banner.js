5fd5657f6c589e13d39c858bca54b1415fe865fb
felippe menezes <felippemenezes1982@gmail.com>
2021-05-21 16:13:10 -0300

    adding style to answer html

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: [">:Ask ^350 your ^350 coach ^350 anything:", ">:Pergunte ^350 qualquer ^350 coisa ^350 ao ^350 seu ^350 coach:"],
+    strings: [">:Ask ^350 your ^350 coach ^350 anything:", ">:Demandez ^350n'importe ^350quoi ^150à ^350votre ^350coach.",">:Pergunte ^350 qualquer ^350 coisa ^350 ao ^350 seu ^350 coach:"],
     typeSpeed: 65,
     backSpeed: 65,
     smartBackspace: true,
