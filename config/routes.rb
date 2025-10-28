55f1b28e24d0e8b83b2e441f8c87952278b8f5aa
Felippe Menezes <felippemenezes1982@gmail.com>
2025-10-28 20:34:04 -0300

    Adding root to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  root "questions#ask"
   
   get "ask", to: "questions#ask", as: :ask
   get "answer", to: "questions#answer", as: :answer
